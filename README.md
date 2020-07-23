6ccd0496b7cc16e8cc89912f131b0045f30e0163
Vivien FABING <vfabing@live.com>
2020-07-23 13:13:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Black Screen PWA
+
+Just a Black Screen to rest your eyes
+
+I used it to focus on my main screen and hide my second screen without cutting the display, but you could use it as you want!
---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open('assets').then(cache => 
+        {
+          return cache.addAll([
+            'index.html',
+            'icons-512.png',
+            'icons-192.png'
+          ])
+        })
+    );
+  });
+  
+  self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
